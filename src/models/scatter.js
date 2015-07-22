d93bc516c76ef4d95bfad91f8bdd08d858912a6b
Hutt Li <hutt@embertec.com>
2015-07-22 14:11:13 +0930

    fix wrong voronoi clip paths on non-chrome browsers

--- src/models/scatter.js
+++ src/models/scatter.js
@@ -219,6 +219,10 @@ nv.models.scatter = function() {
                     if (clipVoronoi) {
                         // voronoi sections are already set to clip,
                         // just create the circles with the IDs they expect
+
+                        // Non-chrome browsers cannot cope well with multiple
+                        // redefined clip paths.
+                        wrap.selectAll("#nv-point-clips").remove();
                         var clips = wrap.append("svg:g").attr("id", "nv-point-clips");
                         clips.selectAll("clipPath")
                             .data(vertices)
